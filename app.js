3f8b95f86e40db380d6d541edc1b052a0ce2e52e
stephen-leisy <stleisy@gmail.com>
2021-01-11 15:38:25 -0800

    refactored added correct answer counter and logged to make sure it works.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+import { AnswerIsYes } from './utils.js';
+
+
+
 const quizButton = document.getElementById('quiz-button');
 const displaySpace = document.getElementById('secret-magic-space');
 
@@ -22,30 +26,19 @@ quizButton.addEventListener('click', () => {
     console.log(firstName);
     console.log(lastName);
 
-
     const firstQuestion = prompt('If you stacked sloths on top of each other would they maybe reach the moon and back? Yes or No.');
 
-    if (firstQuestion.charAt(0).toUpperCase() === "Y") {
-        console.log('You got it!');
-    } else {
-        console.log('nope!');
-    }
-
-    prompt('Do sloths love banana cream pie? Yes or No.');
-
-    if (firstQuestion.charAt(0).toUpperCase() === "Y") {
-        console.log('You got it!');
-    } else {
-        console.log('nope');
-    }
-
-    prompt('Do sloths make eye contact to read human thoughts? Yes or No.');
-
-    if (firstQuestion.charAt(0).toUpperCase() === "Y") {
-        console.log('You got it!');
-    } else {
-        console.log('no way');
-    }
+    if (AnswerIsYes(firstQuestion)) numberOfCorrectAnswers++;
+    console.log(numberOfCorrectAnswers);
+    const secondQuestion = prompt('Do sloths love banana cream pie? Yes or No.');
 
 
-})
+    if (!AnswerIsYes(secondQuestion)) numberOfCorrectAnswers++;
+    console.log(numberOfCorrectAnswers);
+    const thirdQuestion = prompt('Do sloths make eye contact to read human thoughts? Yes or No.');
+
+    if (AnswerIsYes(thirdQuestion)) ++numberOfCorrectAnswers;
+    console.log(numberOfCorrectAnswers);
+
+
+});
---
+++ utils.js
@@ -0,0 +1,4 @@
+
+export function AnswerIsYes(answerString) {
+    return answerString.charAt(0).toUpperCase() === 'Y';
+}
