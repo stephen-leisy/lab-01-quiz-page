f7cbcea33b2b3b39a43860f96a2de8da3c8a2185
stephen-leisy <stleisy@gmail.com>
2021-01-11 16:00:44 -0800

    changed function name to answerIsYes and created test for that function.

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 // import functions and grab DOM elements
 import { AnswerIsYes } from './utils.js';
 
-
-
+// initialize state
 const quizButton = document.getElementById('quiz-button');
 const displaySpace = document.getElementById('secret-magic-space');
-
-// initialize state
-console.log(quizButton);
-console.log(displaySpace);
 // set event listeners to update state and DOM
 
 quizButton.addEventListener('click', () => {
@@ -23,23 +18,18 @@ quizButton.addEventListener('click', () => {
     const firstName = prompt('What is your first name, pal?');
     const lastName = prompt('Say, what be your last name, my new aquaintance?');
 
-    console.log(firstName);
-    console.log(lastName);
-
     const firstQuestion = prompt('If you stacked sloths on top of each other would they maybe reach the moon and back? Yes or No.');
 
     if (AnswerIsYes(firstQuestion)) numberOfCorrectAnswers++;
-    console.log(numberOfCorrectAnswers);
+
     const secondQuestion = prompt('Do sloths love banana cream pie? Yes or No.');
 
-
     if (!AnswerIsYes(secondQuestion)) numberOfCorrectAnswers++;
-    console.log(numberOfCorrectAnswers);
 
     const thirdQuestion = prompt('Do sloths make eye contact to read human thoughts? Yes or No.');
 
     if (AnswerIsYes(thirdQuestion)) ++numberOfCorrectAnswers;
-    console.log(numberOfCorrectAnswers);
+
 
     displaySpace.textContent = `You are a human named ${firstName} ${lastName} and you answered ${numberOfCorrectAnswers} questions correctly with your nifty human brain.`;
 
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,31 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { AnswerIsYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return true if the word starts with y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = AnswerIsYes('yesh');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should return false if the word starts with n', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = AnswerIsYes('nah');
 
     //Expect
     // Make assertions about what is expected versus the actual result
